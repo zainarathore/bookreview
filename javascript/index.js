cdcc0633f126082b323fa51b3dcdee2282073dc2
zainarathore <zaina.rathore@elev.ga.ntig.se>
2025-03-30 19:35:25 +0200

    inlämning bookreview

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     document.querySelector(".menu-links").classList.toggle("show");
   }
   
+  function addReview() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const review = document.getElementById('review').value;
+    if (title && author && review) {
+        const reviewContainer = document.createElement('div');
+        reviewContainer.classList.add('review');
+        reviewContainer.innerHTML = `<strong>${title}</strong> written by ${author}<p>${review}</p>`;
+        document.getElementById('reviews').appendChild(reviewContainer);
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('review').value = '';
+    } else {
+        alert('fill in all boxes before you send your review.');
+    }
+}
